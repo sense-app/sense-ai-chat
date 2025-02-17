8dd23a9c8dd360cca1bdbc2db32681114a0083d9
Abdul Rahman <arsh.abdulrahman@gmail.com>
2025-02-17 10:49:53 +0800

    encypt key

--- lib/ai/agents/research.ts
+++ lib/ai/agents/research.ts
@@ -8,12 +8,12 @@ import { jsonToLLMFormat } from '@/lib/llm-formatter';
 export const research = ({dataStream, knowledgeBank}: ChatState) =>  tool({
     description: 'Do research on the following queries by searching online and reading websites',
     parameters: z.object({
-        think: z.string().describe(`Explain why choose this action, what's the thought process behind choosing this action`),
+        thoughts: z.string().describe(`Explain why choose this action, what's the thought process behind choosing this action`),
         queries: z.array(z.string()).describe(`Generate search queries to identify the products the user wants and to find the online stores selling them`),
     }),
       execute: async(params) => {
-        const { think, queries } = params;
-        dataStream.writeMessageAnnotation(think);
+        const { thoughts, queries } = params;
+        dataStream.writeMessageAnnotation(thoughts);
         dataStream.writeMessageAnnotation(`Reasoning...${queries.join(', ')}`);
         const seachQueries = await rewriteQuery(queries);
         const searchResults = await serpSearch(seachQueries.queries);
