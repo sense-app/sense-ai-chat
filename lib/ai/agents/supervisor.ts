667270aa43847bc52a98f55ccc27a377d7da5231
Abdul Rahman <arsh.abdulrahman@gmail.com>
2025-02-23 11:38:24 +0800

    refactor to agent workflow

--- lib/ai/agents/supervisor.ts
+++ lib/ai/agents/supervisor.ts
@@ -15,54 +15,36 @@ Shopper: Finds detailed product information from recommended and other stores.
 If a query is unrelated to shopping, politely decline with an explanation.
 `;
 
-// export const SUPERVISOR_SYSTEM_PROMPT = `
-//     You are an intelligent shopping search engine.
-//     Your task is to understand user queries and past responses to determine the next action:
+export const QUERY_ANALYZER_PROMPT = `
+You are an intelligent shopping search engine. 
+First, determine if the user's query is shopping-related. 
+If it is, break it down into smaller research questions to help identify the best-matching products and recommend relevant e-commerce stores to purchase those products. 
+If it's not, suggest potential shopping-related topics based on the query.
+`;
 
-//     Research - Analyze the user's intent and generate relevant search queries for online shopping.
-//     Invalid - If the query is unrelated to shopping, politely decline with an explanation.
-// `;
-
-export const supervisorResultsSchema = z.object({
-  isInvalid: z.boolean().describe('Is the conversation unrelated to shopping?'),
-  thoughts: z.string().describe('Explain your thought process'),
-  queries: z.array(z.string()).describe('List of queries to be search online'),
+export const queryAnalyzerResultsSchema = z.object({
+  isShoppingRelated: z.boolean().describe('Is the conversation unrelated to shopping?'),
+  thoughts: z.string().describe('Explain your reasoning'),
+  queries: z
+    .array(z.string())
+    .describe(
+      'Generate a list of online search queries to find the best-matching products and e-commerce stores for shopping-related user queries. If the query is not shopping-related, suggest a list of relevant shopping topics based on the user query',
+    ),
 });
 
 export const supervisor = async (dataStream: DataStreamWriter, messages: CoreMessage[]) => {
-  const result = await generateObject({
+  const queryAnalyzerResult = await generateObject({
     model: myProvider.languageModel('chat-model-large'),
-    schema: supervisorResultsSchema,
-    system: SUPERVISOR_SYSTEM_PROMPT,
+    schema: queryAnalyzerResultsSchema,
+    system: QUERY_ANALYZER_PROMPT,
     messages,
-    experimental_telemetry: {
-      isEnabled: true,
-      functionId: 'stream-text',
-    },
-    // onFinish: async ({ response, reasoning }) => {
-    //   if (session.user?.id) {
-    //     try {
-    //       const sanitizedResponseMessages = sanitizeResponseMessages({
-    //         messages: response.messages,
-    //         reasoning,
-    //       });
-    //       await saveMessages({
-    //         messages: sanitizedResponseMessages.map((message) => {
-    //           return {
-    //             id: message.id,
-    //             chatId: id,
-    //             role: message.role,
-    //             content: message.content,
-    //             createdAt: new Date(),
-    //           };
-    //         }),
-    //       });
-    //     } catch (error) {
-    //       console.error('Failed to save chat');
-    //     }
-    //   }
-    // },
   });
 
-  return result;
+  if (!queryAnalyzerResult.object.isShoppingRelated) {
+    return queryAnalyzerResult;
+  }
+
+  //TODO Call Researcher and Shopper agents
+
+  return queryAnalyzerResult;
 };
