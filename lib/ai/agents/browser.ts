d35425bd03e69e223d91409100bdb57061c989a5
Abdul Rahman <arsh.abdulrahman@gmail.com>
2025-02-26 14:27:21 +0800

    try out midscene.js

--- lib/ai/agents/browser.ts
+++ lib/ai/agents/browser.ts
@@ -1,4 +1,5 @@
 import { chromium } from 'playwright';
+import { PlaywrightAgent } from '@midscene/web';
 
 const browseStore = async (shopSearchTerm: string, searchQueries: string[]) => {
   (async () => {
@@ -6,6 +7,17 @@ const browseStore = async (shopSearchTerm: string, searchQueries: string[]) => {
     // Create pages, interact with UI elements, assert values
     const page = await browser.newPage();
     await page.goto('google.com');
+    const agent = new PlaywrightAgent(page);
+    await agent.aiAction(`type ${shopSearchTerm} into the search bar and hit Enter`);
+    await agent.aiWaitFor(`search results to load on the page`);
+    await agent.aiAction(`click on the ${shopSearchTerm} website link from the search results`);
+    await agent.aiWaitFor(`the e-commerce website to load`);
+
+    for (const searchQuery of searchQueries) {
+      await agent.aiAction(`type ${searchQuery} into the search bar and hit Enter`);
+      await agent.aiWaitFor(`search results to load on the page`);
+      const results = await agent.aiQuery(``);
+    }
 
     await browser.close();
   })();
