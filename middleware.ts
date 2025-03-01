6943d96023633951a2346ba310bc53f79db80aea
Abdul Rahman <arsh.abdulrahman@gmail.com>
2025-03-01 23:26:27 +0800

    exclude static files from middleware

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,19 @@ import { authConfig } from '@/app/(auth)/auth.config';
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: ['/', '/:id', '/api/:path*', '/login', '/register'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\..*|public).*)',
+    '/',
+    '/:id',
+    '/api/:path*',
+    '/login',
+    '/register'
+  ],
 };
